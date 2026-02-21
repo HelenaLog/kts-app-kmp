78a695af8ac0b0f6199e2fd80dd8fdb7d7d1ea3f
Elena <iosdevlogin@gmail.com>
2026-02-21 23:22:09 +0500

    Add OnboardingScreen

---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/screens/onboarding/OnboardingScreen.kt
@@ -0,0 +1,75 @@
+package com.github.helenalog.ktsappkmp.screens.onboarding
+
+import com.github.helenalog.ktsappkmp.ui.components.AppButton
+import com.github.helenalog.ktsappkmp.ui.theme.Dimensions
+import ktsappkmp.composeapp.generated.resources.Res
+import ktsappkmp.composeapp.generated.resources.compose_multiplatform
+import ktsappkmp.composeapp.generated.resources.get_starter
+import ktsappkmp.composeapp.generated.resources.onboarding_title
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import coil3.compose.AsyncImage
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun OnboardingScreen(
+    onNavigateToLogin: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(modifier = modifier) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(all = Dimensions.screenPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            AsyncImage(
+                model = "https://cdni.iconscout.com/illustration/premium/thumb/login-6262949-5253376.png",
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                placeholder = painterResource(Res.drawable.compose_multiplatform),
+                error = painterResource(Res.drawable.compose_multiplatform),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(fraction = 0.5f)
+            )
+            Spacer(modifier = Modifier.height(Dimensions.spacingLarge))
+            Text(
+                stringResource(Res.string.onboarding_title).uppercase(),
+                style = MaterialTheme.typography.headlineMedium,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(Dimensions.spacingXLarge))
+            AppButton(
+                text = stringResource(Res.string.get_starter),
+                onClick = onNavigateToLogin
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun OnboardingScreenPrev() {
+    OnboardingScreen(
+        onNavigateToLogin = {}
+    )
+}
---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/ui/theme/Dimensions.kt
@@ -0,0 +1,13 @@
+package com.github.helenalog.ktsappkmp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimensions {
+    // Screen
+    val screenPadding = 16.dp
+
+    // Spacing
+    val spacingMedium = 16.dp
+    val spacingLarge = 24.dp
+    val spacingXLarge = 32.dp
+}
