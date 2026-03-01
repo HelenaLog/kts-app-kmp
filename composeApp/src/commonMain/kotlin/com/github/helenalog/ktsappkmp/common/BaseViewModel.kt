c91dfc4bd3bf47de0abe3915027113b25cd85e17
Elena <iosdevlogin@gmail.com>
2026-03-01 23:25:07 +0500

    add BaseViewModel

---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/common/BaseViewModel.kt
@@ -0,0 +1,18 @@
+package com.github.helenalog.ktsappkmp.common
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+abstract class BaseViewModel<State, Event>(initialState: State): ViewModel() {
+    val state: StateFlow<State>
+        get() = mutableState.asStateFlow()
+    
+    private val mutableState = MutableStateFlow(initialState)
+
+    fun updateState(block: State.() -> State) {
+        mutableState.update { it.block() }
+    }
+}
