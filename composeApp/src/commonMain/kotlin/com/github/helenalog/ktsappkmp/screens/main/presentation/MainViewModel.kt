e0ea82ddbeb624ffe0e5034f93d85529a3d4406a
Elena <iosdevlogin@gmail.com>
2026-03-02 01:13:43 +0500

    add FriendsRepository

--- composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/screens/main/presentation/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/screens/main/presentation/MainViewModel.kt
@@ -5,13 +5,10 @@ import com.github.helenalog.ktsappkmp.screens.main.presentation.models.Friend
 import com.github.helenalog.ktsappkmp.screens.main.presentation.models.MainUiState
 
 class MainViewModel: BaseViewModel<MainUiState, Nothing>(MainUiState.Initial) {
-
+    private val repository = FriendsRepository()
+    
     fun loadFriends() {
-        val friends = listOf(
-            Friend(1, "Иван", "Иванов", "https://static.vecteezy.com/system/resources/previews/036/463/807/non_2x/ai-generated-young-caucasian-man-in-business-attire-portrait-png.png", 1),
-            Friend(2, "Мария", "Петрова", "https://static.vecteezy.com/system/resources/previews/036/121/731/non_2x/ai-generated-teenage-girl-on-transparent-background-ai-png.png", 0),
-            Friend(3, "Алексей", "Сидоров", null, 1),
-        )
+        val friends = repository.getList()
         updateState { copy(friends = friends) }
     }
 }
