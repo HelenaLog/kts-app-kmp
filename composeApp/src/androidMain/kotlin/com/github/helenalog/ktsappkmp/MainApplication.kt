a417fbff36636036140b1885e41392e257011ef1
Elena <iosdevlogin@gmail.com>
2026-03-15 22:48:25 +0500

    setup DataStore provider

--- composeApp/src/androidMain/kotlin/com/github/helenalog/ktsappkmp/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/com/github/helenalog/ktsappkmp/MainApplication.kt
@@ -3,6 +3,7 @@ package com.github.helenalog.ktsappkmp
 import android.app.Application
 import com.github.helenalog.ktsappkmp.data.storage.SessionProvider
 import com.github.helenalog.ktsappkmp.data.storage.SessionStorageImpl
+import com.github.helenalog.ktsappkmp.data.storage.appContext
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 
@@ -11,5 +12,6 @@ class MainApplication : Application() {
         super.onCreate()
         Napier.base(DebugAntilog())
         SessionProvider.init(SessionStorageImpl(this))
+        appContext = this
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/data/storage/FilesDir.kt
@@ -0,0 +1,3 @@
+package com.github.helenalog.ktsappkmp.data.storage
+
+expect fun getFilesDir(): String
