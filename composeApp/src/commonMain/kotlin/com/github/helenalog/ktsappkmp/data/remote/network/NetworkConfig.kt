b5ba3c1c5939da4d979d16c857a089442a57fd01
Elena <iosdevlogin@gmail.com>
2026-03-23 17:28:36 +0500

    add NetworkConfig, HttpClientFactory, NetworkModule
    # Conflicts:
    #	composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/data/remote/network/Networking.kt
    #	composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/data/repository/CabinetRepositoryImpl.kt

---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/data/remote/network/NetworkConfig.kt
@@ -0,0 +1,8 @@
+package com.github.helenalog.ktsappkmp.data.remote.network
+
+data class NetworkConfig(
+    val sproUrl: String,
+    val authUrl: String,
+    val cabinetId: String,
+    val projectId: String,
+)
