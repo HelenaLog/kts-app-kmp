27049bd04d3e8e95804d8ca912f4a72f22194297
Elena <iosdevlogin@gmail.com>
2026-03-28 18:16:36 +0500

    add ObserveWsMessagesUseCase

--- composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/feature/chat/di/ChatModule.kt
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/feature/chat/di/ChatModule.kt
@@ -2,14 +2,19 @@ package com.github.helenalog.ktsappkmp.feature.chat.di
 
 import com.github.helenalog.ktsappkmp.feature.conversation.presentation.mapper.UserAvatarUiMapper
 import com.github.helenalog.ktsappkmp.core.data.remote.network.NetworkQualifier
+import com.github.helenalog.ktsappkmp.feature.chat.data.mapper.WsMessageMapper
 import com.github.helenalog.ktsappkmp.feature.chat.presentation.mapper.ChatUiMapper
 import com.github.helenalog.ktsappkmp.feature.chat.data.remote.api.ChatApi
+import com.github.helenalog.ktsappkmp.feature.chat.data.remote.api.ChatWebSocketApi
 import com.github.helenalog.ktsappkmp.feature.chat.data.repository.ChatRepositoryImpl
+import com.github.helenalog.ktsappkmp.feature.chat.data.repository.ChatWebSocketRepositoryImpl
 import com.github.helenalog.ktsappkmp.feature.chat.data.repository.ConversationDetailRepositoryImpl
 import com.github.helenalog.ktsappkmp.feature.chat.domain.repository.ChatRepository
+import com.github.helenalog.ktsappkmp.feature.chat.domain.repository.ChatWebSocketRepository
 import com.github.helenalog.ktsappkmp.feature.chat.domain.repository.ConversationDetailRepository
 import com.github.helenalog.ktsappkmp.feature.chat.domain.usecase.GetConversationDetailUseCase
 import com.github.helenalog.ktsappkmp.feature.chat.domain.usecase.GetMessagesUseCase
+import com.github.helenalog.ktsappkmp.feature.chat.domain.usecase.ObserveWsMessagesUseCase
 import com.github.helenalog.ktsappkmp.feature.chat.domain.usecase.SendMessageUseCase
 import com.github.helenalog.ktsappkmp.feature.chat.domain.usecase.UploadAttachmentUseCase
 import com.github.helenalog.ktsappkmp.feature.chat.presentation.ChatViewModel
@@ -19,17 +24,28 @@ import org.koin.dsl.module
 val chatModule = module {
 
     single { ChatApi(get(NetworkQualifier.MAIN)) }
+    single { ChatWebSocketApi(get(NetworkQualifier.MAIN)) }
 
     single<ChatRepository> { ChatRepositoryImpl(api = get()) }
     single<ConversationDetailRepository> { ConversationDetailRepositoryImpl(api = get()) }
+    single<ChatWebSocketRepository> {
+        ChatWebSocketRepositoryImpl(
+            wsClient = get(NetworkQualifier.WS),
+            restApi = get(),
+            networkConfig = get(),
+            mapper = get()
+        )
+    }
 
     factory { GetConversationDetailUseCase(get()) }
     factory { GetMessagesUseCase(get()) }
     factory { SendMessageUseCase(get()) }
     factory { UploadAttachmentUseCase(get()) }
+    factory { ObserveWsMessagesUseCase(get()) }
 
     factory { UserAvatarUiMapper() }
     factory { ChatUiMapper(get()) }
+    factory { WsMessageMapper() }
 
     viewModel {
         ChatViewModel(
