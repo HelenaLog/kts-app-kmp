420069b095091f63c7404013f0bb85b7fd185893
Elena <iosdevlogin@gmail.com>
2026-04-05 17:04:36 +0500

    add bot use cases

---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/feature/chat/domain/usecase/GetScenarioBlocksUseCase.kt
@@ -0,0 +1,9 @@
+package com.github.helenalog.ktsappkmp.feature.chat.domain.usecase
+
+import com.github.helenalog.ktsappkmp.feature.chat.domain.model.ScenarioBlock
+import com.github.helenalog.ktsappkmp.feature.chat.domain.repository.BotActionRepository
+
+class GetScenarioBlocksUseCase(private val repository: BotActionRepository) {
+    suspend operator fun invoke(scenarioId: String): Result<List<ScenarioBlock>> =
+        repository.getBlocks(scenarioId)
+}
