eae0a3e9a51799c1813056ee8156ce1adf4cc87b
Elena <iosdevlogin@gmail.com>
2026-04-06 18:40:09 +0500

    add user list domain model and DTOs

---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/feature/filter/data/remote/dto/UserListResponseDto.kt
@@ -0,0 +1,10 @@
+package com.github.helenalog.ktsappkmp.feature.filter.data.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserListResponseDto(
+    @SerialName("lists")
+    val lists: List<UserListDto>
+)
---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/feature/filter/domain/model/UserList.kt
@@ -0,0 +1,9 @@
+package com.github.helenalog.ktsappkmp.feature.filter.domain.model
+
+import androidx.compose.runtime.Immutable
+
+data class UserList(
+    val id: String,
+    val name: String,
+    val tag: String
+)
