b21e5216ee6b254ec3a583052a38c7c4ce50c08a
Elena <iosdevlogin@gmail.com>
2026-04-13 00:31:20 +0500

    add RemoteConfigBannerRepositoryImpl

---
+++ composeApp/src/commonMain/kotlin/com/github/helenalog/ktsappkmp/core/data/mapper/BannerMapper.kt
@@ -0,0 +1,23 @@
+package com.github.helenalog.ktsappkmp.core.data.mapper
+
+import com.github.helenalog.ktsappkmp.core.data.remote.dto.BannerDto
+import com.github.helenalog.ktsappkmp.core.domain.banner.model.AppBanner
+import com.github.helenalog.ktsappkmp.core.domain.banner.model.BannerAction
+import com.github.helenalog.ktsappkmp.core.domain.banner.model.BannerType
+
+fun BannerDto.toDomain(): AppBanner = AppBanner(
+    id = id,
+    type = type.toBannerType(),
+    message = message,
+    actionLabel = actionText,
+    action = if (!actionUrl.isNullOrBlank()) {
+        BannerAction.OpenUrl(actionUrl)
+    } else {
+        BannerAction.None
+    }
+)
+
+private fun String?.toBannerType(): BannerType = when (this?.lowercase()) {
+    "error" -> BannerType.ERROR
+    else -> BannerType.INFO
+}
